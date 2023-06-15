d5d30c6bc74ccb47f3e023585c8cc2050b0a3fce
imransaglam <imransaglam9@gmail.com>
2023-06-15 23:41:20 +0300

    User process is done

--- OtoServisSatis.Entities/Marka.cs
+++ OtoServisSatis.Entities/Marka.cs
@@ -1,9 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
 namespace OtoServisSatis.Entities
 {
     public class Marka : IEntity
     {
         public int Id { get; set ; }
+        [StringLength(50)]
+        [Display(Name = "Adı"), Required(ErrorMessage = "{0} Boş Bırakılamaz!")]
         public string Adi { get; set; }
     }
 }
--- OtoServisSatis.Entities/Satis.cs
+++ OtoServisSatis.Entities/Satis.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace OtoServisSatis.Entities
 {
     public class Satis:IEntity
     {
         public int Id { get; set; }
+        [Display(Name = "Araç")]
         public int AracId { get; set; }
+        [Display(Name = "Müşteri")]
         public int MusteriId { get; set; }
+        [Display(Name = "Satış Fiyatı")]
         public decimal SatisFiyati { get; set; }
+        [Display(Name = "Satış Tarihi")]
         public DateTime SatisTarihi { get; set; }
+        [Display(Name = "Araç")]
         public virtual Arac? Arac { get; set; }//Satis sınıfı ile Arac sınıfı arasındaki bağlantı 
+        [Display(Name = "Müşteri")]
         public virtual Musteri? Musteri { get; set; }//Satis sınıfı ile Musteri sınıfı arasındaki bağlantı 
     }
 }
