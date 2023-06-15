68903e90094501ffa36b3dd71984a3492b6e8f78
imransaglam <imransaglam9@gmail.com>
2023-06-15 15:59:45 +0300

    roles added

---
+++ OtoServisSatis.Entities/Marka.cs
@@ -0,0 +1,9 @@
+﻿
+namespace OtoServisSatis.Entities
+{
+    public class Marka : IEntity
+    {
+        public int Id { get; set ; }
+        public string Adi { get; set; }
+    }
+}
---
+++ OtoServisSatis.Entities/Satis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtoServisSatis.Entities
+{
+    public class Satis:IEntity
+    {
+        public int Id { get; set; }
+        public int AracId { get; set; }
+        public int MusteriId { get; set; }
+        public decimal SatisFiyati { get; set; }
+        public DateTime SatisTarihi { get; set; }
+        public virtual Arac? Arac { get; set; }//Satis sınıfı ile Arac sınıfı arasındaki bağlantı 
+        public virtual Musteri? Musteri { get; set; }//Satis sınıfı ile Musteri sınıfı arasındaki bağlantı 
+    }
+}
